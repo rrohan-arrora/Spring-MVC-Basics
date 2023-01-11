ed4323ed0947d1d8b951a397169078625cbb00f1
Rohan Arora <mac@macs-MacBook-Air.local>
2023-01-11 22:38:48 +0530

    Added @RequestParam annotation to extract the studentName param

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -22,16 +23,13 @@ public class HelloWorldController {
 	// add method to handle the http request
 	// add data to the model
 	@RequestMapping("/processFormVersion2")
-	public String shoutOut(HttpServletRequest req, Model model) {
-		
-		//read the request parameter from html form
-		String theName = req.getParameter("studentName");
-		
+	public String shoutOut(@RequestParam("studentName") String theName, Model model) {
+				
 		// convert the data to upper case
 		String result = theName.toUpperCase();
 		
 		//create the message
-		result = "YO! "+result;
+		result = "HELLO BABY! "+result;
 		
 		// add the message to the form
 		model.addAttribute("message", result);
